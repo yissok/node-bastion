f29600ca8e01b733dfc7187fe3917996b97e667a
yissok <andreatv12@gmail.com>
2024-02-02 15:02:51 +0000

    Update auth.js

--- Auth/auth.js
+++ Auth/auth.js
@@ -2,10 +2,113 @@ const User = require("../model/User");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
+
 const jwtSecret =
   "4715aed3c946f7b0a38e6b534a9583628d84e96d10fbc04700770d572af3dce43625dd";
+
+require("dotenv").config()
+const nodemailer = require("nodemailer");
+const { google } = require("googleapis");
+const OAuth2 = google.auth.OAuth2;
+
+const createTransporter = async () => {
+  try {
+    const oauth2Client = new OAuth2(
+        process.env.CLIENT_ID,
+        process.env.CLIENT_SECRET,
+        "https://developers.google.com/oauthplayground"
+    );
+
+    oauth2Client.setCredentials({
+      refresh_token: process.env.REFRESH_TOKEN,
+    });
+
+    const accessToken = await new Promise((resolve, reject) => {
+      oauth2Client.getAccessToken((err, token) => {
+        if (err) {
+          console.log("*ERR: ", err)
+          reject();
+        }
+        resolve(token);
+      });
+    });
+
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        type: "OAuth2",
+        user: process.env.USER_EMAIL,
+        accessToken,
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        refreshToken: process.env.REFRESH_TOKEN,
+      },
+    });
+    return transporter;
+  } catch (err) {
+    return err
+  }
+};
+
+const sendMail = async (token) => {
+  try {
+    const mailOptions = {
+      from: process.env.USER_EMAIL,
+      to: "andrea.media.info@gmail.com",
+      subject: "Test",
+      text: "expires in 5 minutes: "+token,
+    }
+
+    let emailTransporter = await createTransporter();
+    await emailTransporter.sendMail(mailOptions);
+  } catch (err) {
+    console.log("ERROR: ", err)
+  }
+};
+
+
+
 exports.register = async (req, res, next) => {
-  const { username, password } = req.body;
+  const { username } = req.body;
+
+  const maxAge = 5 * 60;
+  const token = jwt.sign(
+      { username },
+      jwtSecret,
+      { expiresIn: maxAge }
+  );
+
+  await sendMail(token)//remove whole then part, we want the token to only be available through email because that's the way we can guarantee that one user cannot create infinite accounts
+      .then(() => {
+    res.status(201).json({
+      message: token,
+    })
+  })
+      .catch(error => {
+        console.log("err: "+error)
+      });
+};
+
+
+
+exports.registerPass = async (req, res, next) => {
+  const { token, password } = req.body;
+  jwt.verify(token, jwtSecret, (err, decodedToken) => {
+
+    if (err) {
+      console.log("err: "+err)
+      return res.status(401).json({ message: "Not authorized" });
+    } else {
+      console.log("valid token")
+      console.log("decodedToken: "+decodedToken.username)
+      return res.status(200).json({ message: "Valid token" });
+      // registerUser(password, username)
+    }
+  });
+};
+
+function registerUser(password, username) {
+
   if (password.length < 6) {
     return res.status(400).json({ message: "Password less than 6 characters" });
   }
@@ -14,32 +117,37 @@ exports.register = async (req, res, next) => {
       username,
       password: hash,
     })
-      .then((user) => {
-        const maxAge = 3 * 60 * 60;
-        const token = jwt.sign(
-          { id: user._id, username, role: user.role },
-          jwtSecret,
-          {
-            expiresIn: maxAge, // 3hrs
-          }
-        );
-        res.cookie("jwt", token, {
-          httpOnly: true,
-          maxAge: maxAge * 1000,
-        });
-        res.status(201).json({
-          message: "User successfully created",
-          user: user._id,
-          role: user.role,
-        });
-      })
-      .catch((error) =>
-        res.status(400).json({
-          message: "User not successful created",
-          error: error.message,
+        .then((user) => {
+          const maxAge = 3 * 60 * 60;
+          const token = jwt.sign(
+              { id: user._id, username, role: user.role },
+              jwtSecret,
+              {
+                expiresIn: maxAge, // 3hrs
+              }
+          );
+          res.cookie("jwt", token, {
+            httpOnly: true,
+            maxAge: maxAge * 1000,
+          });
+          res.status(201).json({
+            message: "User successfully created",
+            user: user._id,
+            role: user.role,
+          });
         })
-      );
+        .catch((error) =>
+            res.status(400).json({
+              message: "User not successful created",
+              error: error.message,
+            })
+        );
   });
+}
+
+exports.registerORI = async (req, res, next) => {
+  const { username, password } = req.body;
+  registerUser()
 };
 
 exports.login = async (req, res, next) => {
