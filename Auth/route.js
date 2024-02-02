5c58a8e09579a0ba4d48b69fbff05ca57c4003d3
yissok <andreatv12@gmail.com>
2024-02-02 15:02:27 +0000

    Update route.js

--- Auth/route.js
+++ Auth/route.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const { register, login, update, deleteUser, getUsers } = require("./auth");
+const { registerPass, register, login, update, deleteUser, getUsers } = require("./auth");
 const { adminAuth } = require("../middleware/auth");
 
 router.route("/register").post(register);
+router.route("/registerPass").post(registerPass);
 router.route("/login").post(login);
 router.route("/update").put(adminAuth, update);
 router.route("/deleteUser").delete(adminAuth, deleteUser);
